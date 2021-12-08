61d0f38241306ea9ba4b73c3af58b6e4ba1493cd
Tadeo Rivera <xtecuan@gmail.com>
2021-12-07 20:50:51 -0600

    Build Scripts

--- frun.sh
+++ frun.sh
@@ -4,5 +4,5 @@ export CONTAINER_NAME=sgipd-quarkus
 export CONTAINER_IMAGE=quarkus/sgipd-quarkus
 export CVOLUME=/opt/sigobsol
 
-docker run --privileged --restart unless-stopped  -d -p 7777:7777 -v $CVOLUME:$CVOLUME  --name=$CONTAINER_NAME $CONTAINER_IMAGE
+docker run --privileged --restart unless-stopped  -d -p 7777:7777   --name=$CONTAINER_NAME $CONTAINER_IMAGE
 
