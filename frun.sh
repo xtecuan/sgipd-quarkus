159e833db55c35a0db05254833fad84c47d45338
Tadeo Rivera <xtecuan@gmail.com>
2021-12-07 21:43:24 -0600

    Build Scripts

--- frun.sh
+++ frun.sh
@@ -3,6 +3,7 @@
 export CONTAINER_NAME=sgipd-quarkus
 export CONTAINER_IMAGE=quarkus/sgipd-quarkus
 export CVOLUME=/opt/sigobsol
+export SGIPD_HOST="sgipd.mined.gob.sv:192.168.36.157"
 
-docker run --privileged --restart unless-stopped  -d -p 7777:7777   --name=$CONTAINER_NAME $CONTAINER_IMAGE
+docker run --privileged --restart unless-stopped  -d -p 7777:7777 --add-host $SGIPD_HOST  --name=$CONTAINER_NAME $CONTAINER_IMAGE
 
