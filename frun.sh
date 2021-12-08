a01eda36a36a3574f8dd802b4eb523c04d02033f
Tadeo Rivera-Pineda <xtecuan@protonmail.com>
2021-12-07 23:17:08 -0600

    cambios

--- frun.sh
+++ frun.sh
@@ -6,5 +6,6 @@ export CVOLUME=/opt/sigobsol
 export SGIPD_HOST="sgipd.mined.gob.sv:172.17.0.1"
 export POD_NET=network-keycloak
 
-docker run --privileged --restart unless-stopped  -d -p 80:7777 --add-host $SGIPD_HOST --net=$POD_NET  --name=$CONTAINER_NAME $CONTAINER_IMAGE
+#docker run --privileged --restart unless-stopped  -d -p 80:7777 --add-host $SGIPD_HOST --net=$POD_NET  --name=$CONTAINER_NAME $CONTAINER_IMAGE
+docker run --privileged --restart unless-stopped  -d -p 192.168.36.157:80:7777   --name=$CONTAINER_NAME $CONTAINER_IMAGE
 
